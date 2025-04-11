80b2e4b0c4dd67f518713acacf399393be525550
Okeanalysis <emmanueloke760@gmail.com>
2025-04-11 19:07:18 +0100

    update

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sqlite3
 
 app = Flask(__name__)
 
+
+
 def init_db():
     conn = sqlite3.connect("specimens.db")
     cursor = conn.cursor()
@@ -52,4 +54,6 @@ def index():
     return render_template("index.html", result=result, records=all_records)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
